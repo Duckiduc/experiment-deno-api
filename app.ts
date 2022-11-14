ff7ac357486750e00a081f63edd74e1201009a2d
Duc-Thomas <duckiduc.dev@gmail.com>
2022-11-14 12:14:04 +0100

    Add information to basic response

--- app.ts
+++ app.ts
@@ -1,7 +1,15 @@
 import { serve } from 'https://deno.land/std@0.136.0/http/server.ts'
 
 function requestHandler() {
-  return new Response("Hello world!")
+  const body = JSON.stringify({ message: 'Successfully created' })
+
+  // This response is delivered on every path
+  return new Response(body, {
+    status: 201,
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
 }
   
 serve(requestHandler)
