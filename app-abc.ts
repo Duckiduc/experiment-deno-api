94b049be26a17ace3f984647630ce2e8146bd68b
Duc-Thomas <duckiduc.dev@gmail.com>
2022-11-14 17:52:08 +0100

    get Users from json file

--- app-abc.ts
+++ app-abc.ts
@@ -1,4 +1,5 @@
 import { Application, Context } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import User from "./interfaces/user.ts";
 
 // Using abc for API routes
 
@@ -7,6 +8,7 @@ const app = new Application();
 const PORT = 8080;
 
 async function createUser(ctx: Context) {
+  // Body of request
   const user = await ctx.body;
   // Insert DB POST here
 
@@ -16,12 +18,25 @@ async function createUser(ctx: Context) {
 app.post("/user", createUser);
 
 async function getUsers(ctx: Context) {
-  const users = await Deno.readTextFile('./users.json');
+  // Params inside request url
+  const { id } = ctx.params
 
-  return ctx.json({ users, message: "Users"}, 200);
+  // Reading files with Deno
+  const users: User[] = JSON.parse(await Deno.readTextFile('./users.json'))
+
+  // Params are always string formatted as it is extracted from an url
+  const userId = parseInt(id)
+
+  const user = users.find((u) => u.id === userId)
+
+  if (user) {
+    return ctx.json({ user, message: 'Success' }, 200)
+  } else {
+    return ctx.json({ message: 'User not found' }, 404)
+  }
 }
 
-app.get("/users", getUsers);
+app.get("/users/:id", getUsers);
 
 function getUser(ctx: Context) {
   const { id } = ctx.params;
